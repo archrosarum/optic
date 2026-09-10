b9232a7b3e42b64a1b7be68212f94a76f1c121f7
andrekpsiew <andrewsalois09@gmail.com>
2026-09-10 18:50:18 -0400

    change the src

--- src/hello.asm
+++ src/hello.asm
@@ -1,22 +1,9 @@
-section .data
-    ; Our string layout: 13 characters + a newline character (10)
-    msg db "Hello, World!", 10    
-    ; Calculate length of the string dynamically
-    len equ $ - msg                
-
 section .text
-    global _start
+global _start
 
 _start:
-    ; 1. sys_write(stdout, msg, len)
-    mov eax, 4      ; System call number for 32-bit sys_write
-    mov ebx, 1      ; 1st arg (EBX): File descriptor 1 is stdout
-    mov ecx, msg    ; 2nd arg (ECX): Pointer to our text string
-    mov edx, len    ; 3rd arg (EDX): Length of the string (14 bytes)
-    int 0x80        ; Trigger the Linux kernel handler interrupt
+    mov eax, 0          ; Initialize our number to 0
 
-    ; 2. sys_exit(0)
-    mov eax, 1      ; System call number for 32-bit sys_exit
-    mov ebx, 0      ; 1st arg (EBX): Return exit code 0 (success)
-    int 0x80        ; Trigger the Linux kernel handler interrupt
-
+.loop:
+    inc eax             ; Add 1 to the number in EAX
+    jmp .loop           ; Jump back to the '.loop' label forever
